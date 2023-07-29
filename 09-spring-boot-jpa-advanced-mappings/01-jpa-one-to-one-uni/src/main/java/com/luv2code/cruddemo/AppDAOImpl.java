810fa78513a00811ebbb99520102edb4e70e3a2a
guehara-code <gil_uehara@yahoo.com.br>
2023-07-29 08:26:15 -0300

    more studies

---
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/AppDAOImpl.java
@@ -0,0 +1,30 @@
+package com.luv2code.cruddemo;
+
+import com.luv2code.cruddemo.DAO.AppDAO;
+import com.luv2code.cruddemo.entity.Instructor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AppDAOImpl implements AppDAO {
+
+    // define field for entity manager
+    private EntityManager entityManager;
+
+    // inject entity manager using constructor injection
+
+    @Autowired
+    public AppDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Instructor theInstructor) {
+        entityManager.persist(theInstructor);
+    }
+
+}
---
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/DAO/AppDAO.java
@@ -0,0 +1,7 @@
+package com.luv2code.cruddemo.DAO;
+
+import com.luv2code.cruddemo.entity.Instructor;
+
+public interface AppDAO {
+    void save(Instructor theInstructor);
+}
