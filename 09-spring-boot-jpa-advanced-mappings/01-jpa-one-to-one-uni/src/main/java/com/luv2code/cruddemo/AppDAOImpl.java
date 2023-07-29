308abab837bafa5b0828d2accd8f1558e6f1905a
guehara-code <gil_uehara@yahoo.com.br>
2023-07-29 09:22:57 -0300

    more studies

--- 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/AppDAOImpl.java
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/AppDAOImpl.java
@@ -27,4 +27,22 @@ public class AppDAOImpl implements AppDAO {
         entityManager.persist(theInstructor);
     }
 
+    @Override
+    public Instructor findInstructorById(int theId) {
+        return entityManager.find(Instructor.class, theId);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorById(int theId) {
+
+        // retrieve the instructor
+        Instructor tempInstructor = entityManager.find(Instructor.class, theId);
+
+        // delete the instructor
+        entityManager.remove(tempInstructor);
+
+    }
+
+
 }
--- 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/DAO/AppDAO.java
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/DAO/AppDAO.java
@@ -1,7 +1,13 @@
 package com.luv2code.cruddemo.DAO;
 
 import com.luv2code.cruddemo.entity.Instructor;
+import com.luv2code.cruddemo.entity.InstructorDetail;
 
 public interface AppDAO {
     void save(Instructor theInstructor);
+
+    Instructor findInstructorById(int theId);
+
+    void deleteInstructorById(int theId);
+
 }
