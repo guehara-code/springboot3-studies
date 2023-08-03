312a15b9600b17b6d0a0077b5febd084c1f63f8b
guehara-code <gil_uehara@yahoo.com.br>
2023-08-03 07:35:53 -0300

    more studies

--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -9,15 +9,27 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyDemoLoggingAspect {
 
-    // this is where we add all of our related advices for logging
-
-    // let's start with an @Before advice
-
     @Pointcut("execution(* com.luv2code.aopdemo.DAO.*.*(..))")
     private void forDaoPackage() {}
 
-    @Before("forDaoPackage()")
+    // create a pointcut for getter methods
+    @Pointcut("execution(* com.luv2code.aopdemo.DAO.*.get*(..))")
+    private void getter() {}
+
+    // create a pointcut for the setter methods
+    @Pointcut("execution(* com.luv2code.aopdemo.DAO.*.set*(..))")
+    private void setter() {}
+
+    // create pointcut: include package ... exclude getter/setter
+    @Pointcut("forDaoPackage() && !(getter() || setter())")
+    private void forDaoPackageNoGetterSetter() {}
+
+    @Before("forDaoPackageNoGetterSetter()")
     public void beforeAddAccountAdvice() {
         System.out.println("\n=====>>> Executing @Before advice on method()");
+    }
+
+    public void performApiAnalytics() {
+        System.out.println("\n=====>>> Perfoming API analytics");
     }
 }
