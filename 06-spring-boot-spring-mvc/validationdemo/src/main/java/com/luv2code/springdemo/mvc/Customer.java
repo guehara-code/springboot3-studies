0a3f4424bd91c49b829a36b69d1c22c314f2dde8
guehara-code <gil_uehara@yahoo.com.br>
2023-08-10 08:33:13 -0300

    more studies

--- 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
+++ 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
@@ -1,5 +1,6 @@
 package com.luv2code.springdemo.mvc;
 
+import com.luv2code.springdemo.mvc.validation.CourseCode;
 import jakarta.validation.constraints.*;
 
 public class Customer {
@@ -12,11 +13,22 @@ public class Customer {
 
     @NotNull(message="is required")
     @Min(value=0, message="must be greater than or equal to zero")
-    @Max(value=0, message="must be lower than or equal to 10")
+    @Max(value=10, message="must be lower than or equal to 10")
     private Integer freePasses;
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
     private String postalCode;
+
+    @CourseCode
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    private String courseCode;
 
     public String getFirstName() {
         return firstName;
